43c502c5f8f1983fe35db15d4d8695e28ceda94c
DarksChoice <andradecardozojd@gmail.com>
2023-07-22 16:02:31 -0500

    Deploy v2

--- comcheck/settings.py
+++ comcheck/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure--@)he8nt@^ybps6g@nhn6b^))0x_z0#fum5pp*z6+u%p68*942
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,9 +40,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rest_framework',
+    'corsheaders',
+
+    
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CORS_ORIGIN_ALLOW_ALL = True
+
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- comcheck/urls.py
+++ comcheck/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from app.views import GenerateXMLView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
     path('generate-xml/',GenerateXMLView.as_view()),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
