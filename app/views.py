75f4a4c5169cc5d6e8ff65bcd1c3a7236f2349e9
DarksChoice <andradecardozojd@gmail.com>
2023-07-24 14:38:07 -0500

    Deploy V14

--- app/views.py
+++ app/views.py
@@ -30,10 +30,17 @@ class ScrapePDFViewSet(viewsets.ModelViewSet):
             output_filename =f'{PDF_SCRAPED_FILE}'
             pdfs_folder ='media/pdfs'
             input_filename = os.path.basename(file_path)  
-                      
             space_input_filename = os.path.join(pdfs_folder, input_filename)     
-                
             PDFScraper.main(space_input_filename, output_filename)
+            
+            for filename in os.listdir(pdfs_folder):
+                if filename != input_filename:
+                    file_path = os.path.join(pdfs_folder, filename)
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        os.remove(file_path)
+                        
 
             return Response({'status': 'success', 'message': 'PDF scraped successfully'}, status=status.HTTP_201_CREATED)
         else:
