c9890ef1348db95f94279424d7663575862db4ed
founderXant <anthony@ai4mep.com>
2025-08-30 12:55:36 -0400

    -I'm a faliure. my life sucks. I can't even deploy one simple website. FML noone will ever read this dumb shit I hate that I have to fucking leave a message every time I want to change some shit on my website. I don't understand why this is not working. FML KMS. I just want to make a simple update to my website. Why is it so fucking hard to even keep some shit running without changing it and then adding more to it. THis website will never work if i don't do daily updates and if it takes me  fucking year to make an update this shit will never fucking work I'm so tired I just want to update my website with my new program fml. TLDR: minor updates for stability.

--- app/views/scrapping_pdf.py
+++ app/views/scrapping_pdf.py
@@ -21,7 +21,7 @@ class ScrapePDFViewSet(viewsets.ModelViewSet):
         standard = request.data.get('standard')
 
         if file:
-            pdf = PDFDocument.objects.create(file=file, standard=standard)
+            pdf = PDFDocument.objects.create(file=file,)
             file_path = pdf.file.path
 
             output_filename = f'{PDF_SCRAPED_FILE}'
